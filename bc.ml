b95eaf7c608807316e841ba9a5a72c7b760258ed
Jose Rivera <joserivera4497@gmail.com>
2019-03-24 14:55:53 -0400

    fixed str opt error in fctdef

--- bc.ml
+++ bc.ml
@@ -1,4 +1,6 @@
 open Printf
+open Core
+
 
 type sExpr =
     | Atom of string
@@ -22,32 +24,175 @@ type statement =
 
 type block = statement list
 
-type env = string * float
+type env =
+    | Number of float
+    | Variable of string * float
+    | Function of string * string list * block
 
 type envQueue = env list
 
-let searchQueue (_v: string) (q:envQueue): env =
-	match _v with
-	| v -> List.find(fun s -> fst s = v) q
-	| _ -> raise (Failure "not found")
+let getID (_n:env): string =
+    match _n with
+    | Variable(s, f) -> s
+    | Function(s, sl, b) -> s
+    | Number(f) -> ""
+    | _ -> ""
+
+let getValue (_n:env): float =
+    match _n with
+    | Variable(s, f) -> f
+    | Number(f) -> f
+    | Function(s, sl, b) -> 0.0
+    | _ -> 0.0
+
+let rec existsInQueue (_v:string) (_q:envQueue): bool =
+    match _q with
+    | [] -> false
+    | n::tl -> (
+        if getID n = _v then true
+        else existsInQueue _v tl
+    )
+
+let rec searchQueue (_v:string) (_q:envQueue): env =
+    match _q with
+    | [] -> raise (Failure "Variable not in environment")
+    | n::tl -> (
+        if getID n = _v then n
+        else searchQueue _v tl
+    )
+
+let rec getIndex (_n:env) (_q:envQueue): int =
+    match _q with
+    | [] -> raise (Failure "Variable not in environment")
+    | h :: t -> if _n = h then 0 else 1 + getIndex _n t
+
+let rec list_swap l u v =
+  match l with
+    | [] -> []
+    | h::t -> (if h = u then v
+                else if h = v then u
+                  else h)::(list_swap t u v)
+
+let removeEl (l:envQueue) (i:int): envQueue =
+    match l with
+    | [] -> []
+    | lst -> let front = (List.slice l 0 i) in
+            let back = (List.slice l (i+1) 0) in
+            front@back
+
+(* Test for expression *)
+let%expect_test "searchQueueTest" =
+    searchQueue "v" [Variable("v", 1.0)] |>
+    getValue |>
+    printf "%f";
+    [%expect {| 1.0 |}]
 
 let varEval (_v:string) (_q:envQueue): float  =
-	match _v with
-	| v -> (
-		let e = searchQueue v _q in
-		snd e
-		)
-	| _ -> 0.0
+    match _v with
+    | v -> (
+        let e = searchQueue v _q in
+        getValue e
+    )
+    | _ -> 0.0
 
-let rec evalExpr (_e:expr) (_q:envQueue): float  =
+let string_of_str_opt (v): string =
+	match v with
+		| Some str -> str
+	    | None -> raise(Failure "string_of_string_opt, no string")
+
+let rec evalCode (_code: block) (_q:envQueue): envQueue =
+    match _code with
+        | [] -> ( (* no more code to be evaulauted *)
+            Printf.printf ""; _q
+        )
+        | st::tail -> ( (* eval next statement in list *)
+            let qq = evalStatement st _q in
+                evalCode tail qq
+        )
+        | _ -> print_endline "0"; _q
+
+and evalStatement (s: statement) (q:envQueue): envQueue =
+    match s with
+        | Assign(_v, _e) -> (
+            let x = evalExpr _e q in
+                if existsInQueue _v q then(
+                    let newList = removeEl q (getIndex (searchQueue _v q) q) in
+                    let newEnv:env = Variable(_v, x) in
+                        newEnv::newList
+                    )
+                else
+                    let newEnv:env = Variable(_v, x) in
+                        newEnv::q
+        )
+        (*| Return(_e) -> (
+            (evalExpr _e q); q
+            (*pop environment?*)
+        )*)
+        | Expr(_e) -> (
+            let exprResult = evalExpr _e q in
+            Printf.printf "%f\n" exprResult; q
+        )
+        | If(e, codeT, codeF) -> (
+            let cond = evalExpr e q in
+            if cond > 0.0 then
+                evalCode codeT q
+            else evalCode codeF q
+        )
+        | While(e, code) -> (
+            let qq = ref [] in (
+                let cond = ref (evalExpr e q) in (
+                    if (!cond>0.0) then(
+                        qq := (evalCode code q);
+                        cond := (evalExpr e !qq)
+                    );
+                    while (!cond>0.0) do
+                        qq := evalCode code !qq;
+                        cond := evalExpr e !qq
+                    done; !qq
+                )
+            )
+        )
+        | For(s, e, st, code) -> (
+            let new_q = evalStatement s q in
+                let qq = ref new_q in
+                    let cond = ref (evalExpr e !qq) in
+                        while (!cond>0.0) do
+                            qq := evalCode code !qq;
+                            qq := evalStatement st !qq;
+                            cond := evalExpr e !qq
+                        done; !qq
+        )
+
+        | FctDef(s, params, code) -> (
+            if (existsInQueue s q) then (
+                let newList = (removeEl q (getIndex (searchQueue s q) q)) in
+                let qq = ref newList in
+                    for x = 0 to (List.length params) do (
+                    let i = List.nth params x in
+					let iStr = string_of_str_opt i in
+                        qq := (evalStatement (Assign(iStr, Num(0.0))) !qq);
+                    )
+                    done;
+                    let newFunc:env = Function(s, params, code) in
+                        newFunc::!qq
+
+            )
+            else (
+                let newFunc:env = Function(s, params, code) in
+                    newFunc::q
+            );
+        )
+        | _ -> q (*ignore *)
+
+and evalExpr (_e:expr) (_q:envQueue): float  =
     match _e with
     | Num(e) -> e
     | Var(e) -> varEval e _q
     | Op1(op, x) -> (
         match op with
-        | "++" -> evalExpr x _q +. 1.
+        | "++" -> evalExpr x _q +. 1. (* send to evalStatement *)
         | "--" -> evalExpr x _q -. 1.
-        | "!"  -> if evalExpr x _q != 0.0 then 1. else 0.
+        | "!"  -> if Float.abs (evalExpr x _q) > 0.0 then 1. else 0.
         | _ -> 0.0
     )
     | Op2(op, x, y) -> (
@@ -58,48 +203,46 @@ let rec evalExpr (_e:expr) (_q:envQueue): float  =
         | "-" -> evalExpr x _q -. evalExpr y _q
         | "*" -> evalExpr x _q ** evalExpr y _q
         | "==" -> if compare (evalExpr x _q) (evalExpr y _q)=0 then 1. else 0.
-        | "!=" -> if compare (evalExpr x _q) (evalExpr y _q)!=0 then 1. else 0.
+        | "!=" -> if abs (compare (evalExpr x _q) (evalExpr y _q))>0 then 1. else 0.
         | "<" -> if compare (evalExpr x _q) (evalExpr y _q)<0 then 1. else 0.
         | "<=" -> if compare (evalExpr x _q) (evalExpr y _q)<=0 then 1. else 0.
         | ">" -> if compare (evalExpr x _q) (evalExpr y _q)>0 then 1. else 0.
         | ">=" -> if compare (evalExpr x _q) (evalExpr y _q)>=0 then 1. else 0.
-        | "&&" -> if compare (evalExpr x _q) (evalExpr y _q)!=0 then 1. else 0.
+        | "&&" -> if abs (compare (evalExpr x _q) (evalExpr y _q))>0 then 1. else 0.
         | _ -> 0.0
     )
-    | Fct(name, xs) -> 0.0
+    | Fct(name, xs) -> 336.0 (*)(
+        if existsInQueue name _q then(
+
+        )
+        else
+            raise (Failure "Funtion not defined")
+    )*)
+
+let rec searchAndReplace (_v:string) (_e:expr) (_q:envQueue): envQueue =
+    match _q with
+    | [] -> raise (Failure "Variable not in environment")
+    | n::tl -> (
+        if getID n = _v then
+            let x = evalExpr _e _q in
+            let newEnv:env = Variable(_v, x) in
+            list_swap _q n newEnv
+        else searchAndReplace _v _e tl
+    )
+
 
 (* Test for expression *)
 let%expect_test "evalNum" =
-    evalExpr (Op2(">", Num(11.0), Num(10.0))) [] |>
+    evalExpr (Num 10.0) [] |>
     printf "%F";
-    [%expect {| 1. |}]
-
-let evalCode (_code: block) (_q:envQueue): unit =
-    (* crate new environment *)
-    (* user fold_left  *)
-    (* pop the local environment *)
-    print_endline "Not implemented"
-
-let evalStatement (s: statement) (q:envQueue): envQueue =
-    match s with
-        | Assign(_v, _e) -> (
+    [%expect {| 10. |}]
 
 
-            let x = evalExpr _e q in
-            let newEnv:env = _v, x in
-                newEnv :: q
-		)
-		|Expr(_e) -> (
-			print_float (evalExpr _e q); q
-		)
-        | If(e, codeT, codeF) ->
-            let cond = evalExpr e q in
-                if(cond>0.0) then
-                    evalCode codeT q
-                else
-                    evalCode codeF q
-            ;q
-        | _ -> q (*ignore *)
+(* Test for nested expresions *)
+(*let%expect_test "evalNum" =
+    evalExpr (Op2("-", Num 40.0, Op2("+", Num 20.0, Num 10.0)) [] |>
+    printf "%F";
+    [%expect {| 10. |}]*)
 
 
 (*
@@ -114,11 +257,9 @@ let p1: block = [
 ]
 
 	let%expect_test "p1" =
-		let e:string = "a" in
-		let q:float = 2.0 in
-		let newEnv: env = e, q in
-		    evalStatement (List.hd p1) [newEnv];
-		    [%expect {| 1. |}]
+		let _ = (evalCode p1 []) in print_endline "";
+        [%expect {| 1. |}]
+
 
 (*
     v = 1.0;
@@ -138,7 +279,7 @@ let p2: block = [
         [For(
             Assign("i", Num(2.0)),
             Op2("<", Var("i"), Num(10.0)),
-            Expr(Op1("++a", Var("i"))),
+            Assign("i", Op2("+", Var("i"), Num(1.0))),
             [
                 Assign("v", Op2("*", Var("v"), Var("i")))
             ]
@@ -147,9 +288,26 @@ let p2: block = [
     Expr(Var("v"))
 ]
 
-let%expect_test "p1" =
-    evalCode p2 [];
+let%expect_test "p2" =
+    let _ = evalCode p2 [] in print_endline "";
     [%expect {| 3628800. |}]
+
+
+(*While test*)
+let p4: block = [
+    Assign("v", Num(5.0));
+    While(
+        Op2("<", Var("v"), Num(10.0)),
+        [
+            Assign("v", Op2("+", Var("v"), Num(1.0)));
+        ]
+    );
+    Expr(Var("v"))
+]
+
+let%expect_test "p4" =
+    let _ = evalCode p4 [] in print_endline "";
+    [%expect {| 10. |}]
 
 (*  Fibbonaci sequence
     define f(x) {
@@ -158,11 +316,10 @@ let%expect_test "p1" =
         else
             return (f(x-1)+f(x-2))
     }
-
     f(3)
     f(5)
  *)
- (*)
+
 let p3: block =
     [
         FctDef("f", ["x"], [
@@ -177,12 +334,10 @@ let p3: block =
         Expr(Fct("f", [Num(3.0)]));
         Expr(Fct("f", [Num(5.0)]));
     ]
-
 let%expect_test "p3" =
     evalCode p3 [];
+	let _ = evalCode p3 [] in print_endline "";
     [%expect {|
         2.
         5.
     |}]
-
-*)
